3b01b14fbd4b2af78941ff1eca291fbfd2de513f
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-07-22 00:10:23 +0700

    Cài đặt PUG

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
-import express,{ Express , Request , Response} from "express";
-
+import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
 const port: number = 3000;
 
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "pug");
 
-app.get("/tours",(req: Request, res:Response)=>{
-    res.send("Danh sách tour")
-})
 
+app.get("/tours", (req: Request, res: Response) => {
+  res.render("client/pages/tours/index.pug");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
